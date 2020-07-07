f3e1fb5efb3eb1d11c45b68241e43a673ab64c85
sunyabin <sunyabin89@msn.cn>
2020-07-07 10:19:48 +0800

    添加项目文件。

---
+++ Working/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace Working
+{
+    public class Program
+    {
+        [Obsolete]
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder.ConfigureNLog("Nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "stop the program due to the exception");
+                throw;
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+            .UseNLog();
+    }
+}
